02705619fd574518fb44a0dc08d51ee6f287a7fe
yusufabedin <yusuf.abedin95@gmail.com>
2021-09-23 16:58:47 +0600

    home section end

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var type = new Typed(".typing-text", {
+  strings: [
+    "web designer",
+    "front end developer",
+    "graphic designer",
+    "photographer",
+  ],
+  typeSpeed: 120,
+  loop: true,
+});
